7bde70daffa0b5df4e440bcfe61427366a3bfd82
Santu Mondal <santu.mondal@brainiuminfotech.com>
2024-10-24 01:41:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,198 @@
+// const App = () => {
+//   console.log('Hello from component')
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(now, a+b)
+
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+// const Hello = () => {
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       <p>Hello {props.name}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+
+//       <Hello />
+//       <Hello />
+//       <Hello />
+//     </div>
+//   )
+// }
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name='George' />
+//       <Hello name='Daisy' />
+//     </div>
+//   )
+// }
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name='Maya' age={26 + 10} />
+//       {/* <Hello name={name} age={age} /> */}
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <>
+//       <h1>Greetings</h1>
+//       <Hello name='Maya' age={26 + 10} />
+//       <Hello name={name} age={age} />
+//       <Footer />
+//     </>
+//   )
+// }
+
+
+// export default App
+
+
+
+// const App = () => {
+//   const friends = [
+//     { name: 'Peter', age: 4 },
+//     { name: 'Maya', age: 10 },
+//   ]
+
+//   return (
+//     <div>
+//       <p>{friends[0]}</p>
+//       <p>{friends[1]}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const friends = [
+//     { name: 'Peter', age: 4 },
+//     { name: 'Maya', age: 10 },
+//   ]
+
+//   return (
+//     <div>
+//       <p>{friends[0].name} {friends[0].age}</p>
+//       <p>{friends[1].name} {friends[1].age}</p>
+//     </div>
+//   )
+// }
+
+
+// const App = () => {
+//   const friends = [ 'Peter', 'Maya']
+
+//   return (
+//     <div>
+//       <p>{friends}</p>
+//     </div>
+//   )
+// }
+
+// import { useState } from 'react'
+
+// const App = () => {
+//   // save clicks of each button to its own state
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+
+//   return (
+//     <div>
+//       code here
+//     </div>
+//   )
+// }
+// export default App
+
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <div>
+      {anecdotes[selected]}
+    </div>
+  )
+}
+
+export default App
